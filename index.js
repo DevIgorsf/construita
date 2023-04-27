083c0a60f3f7461a019cfc0346c5842b82f63d95
Igor Soares Fernandes <iigorsf.dev@gmail.com>
2023-04-27 18:43:35 -0300

    Modal de galeria e copyright

--- index.js
+++ index.js
@@ -31,3 +31,44 @@ function galeriaShowSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var ModalSlideIndex = 1;
+showSlides(ModalSlideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(ModalSlideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(ModalSlideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {ModalSlideIndex = 1}
+  if (n < 1) {ModalSlideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[ModalSlideIndex-1].style.display = "block";
+  dots[ModalSlideIndex-1].className += " active";
+  captionText.innerHTML = dots[ModalSlideIndex-1].alt;
+}
